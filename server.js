173b58d4f1183727b9693bc58afa9abf08b16a0a
hanif <hanif314@gmail.com>
2016-02-06 16:49:16 -0500

    can post

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 var config = require('./config/config.js'),
     mongoose = require('mongoose'),
     express = require('express'),
-    morgan = require('morgan');
+    morgan = require('morgan'),
+    bodyparser = require('body-parser'),
     app = express();
 
-
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static('view'));
 app.get('/' , function(req , res){
 	res.sendFile('/index.html');
 });
 
 app.post('/createCustomer', function(req, res) {
-  var username = req.body.username,
+
+    var username = req.body.username;
+    console.log(username);
+    res.send(username);
+    /*
+     var username = req.body.username,
       password = req.body.password,
       id = req.body.id,
       name = req.body.name,
@@ -20,6 +27,8 @@ app.post('/createCustomer', function(req, res) {
       subcontractor = req.body.subcontractor,
       budget = req.body.budget,
       events = req.body.events;
+
+     */      
 });
 
 app.listen(config.port, function() {
