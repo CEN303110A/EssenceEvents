4df53e6f6daeca2786f3d18ac1fa439735de83cc
WilliamWickerson <willie7296@gmail.com>
2016-02-06 13:56:31 -0500

    unnecessary array

--- model/gallerySchema.js
+++ model/gallerySchema.js
@@ -2,13 +2,11 @@ var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
 var gallerySchema = new Schema({
-  gallery: [{
-    name: String,
-    count: Number,
-    photos: [{
-      data: Buffer,
-      contentType: String
-    }]
+  name: String,
+  count: Number,
+  photos: [{
+    data: Buffer,
+    contentType: String
   }]
 });
 
