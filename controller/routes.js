53676a27cc3b903f05e375d462c67ccbc02cf07e
Chase Farmer <chasefarmer2808@ufl.edu>
2016-02-06 16:27:55 -0500

    create customer

--- controller/routes.js
+++ controller/routes.js
@@ -1,7 +1,7 @@
 var express = require('express'),
-    
+
     router = express.Router();
-  
+
 
 exports.home = function(req, res){
 	res.sendFile('public/index1.html' , { root : __dirname});
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var config = require('./config/config.js'), 
-    mongoose = require('mongoose'),   
+var config = require('./config/config.js'),
+    mongoose = require('mongoose'),
     express = require('express'),
     morgan = require('morgan');
     app = express();
@@ -10,6 +10,18 @@ app.get('/' , function(req , res){
 	res.sendFile('/index.html');
 });
 
-app.listen(3000, function() {
-	  console.log('Example app listening on port 3000!');
+app.post('/createCustomer', function(req, res) {
+  var username = req.body.username,
+      password = req.body.password,
+      id = req.body.id,
+      name = req.body.name,
+      phoneNumber = req.body.phoneNumber,
+      email = req.body.email,
+      subcontractor = req.body.subcontractor,
+      budget = req.body.budget,
+      events = req.body.events;
+});
+
+app.listen(config.port, function() {
+	  console.log('Example app listening on port ', config.port, '!');
 });
